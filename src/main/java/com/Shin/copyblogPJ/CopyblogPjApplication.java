09e3cb7cd018d1939a2d90bbd49e5b447bd8b803
shin <sshs1552@naver.com>
2025-05-06 15:18:32 +0900

    first commit

---
+++ src/main/java/com/Shin/copyblogPJ/CopyblogPjApplication.java
@@ -0,0 +1,13 @@
+package com.Shin.copyblogPJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyblogPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyblogPjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shin/copyblogPJ/CopyblogPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shin.copyblogPJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyblogPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
